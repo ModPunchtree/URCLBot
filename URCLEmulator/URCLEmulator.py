c44c1bd5ee4e8b8f8399d0146ac2cd2def0e1693
Mod Punchtree <modpunchtree@gmail.com>
2021-04-22 12:49:38 +0100

    Added URCL Emulator Module.

---
+++ URCLEmulator/URCLEmulator.py
@@ -0,0 +1,3 @@
+
+def emulate(code: str) -> str:
+    return ""
--- _main.py
+++ _main.py
@@ -4,6 +4,6 @@ from bCompiler.bCompiler import compile
 from MPU6Transpiler.MPU6Transpiler import MPU6Transpile
 
 f = open("offlineInput.txt", "r+")
-code = f.readlines()
+code = f.read()
 
-print(compile(code))
+print(compile(code[2:], 8, 2))
